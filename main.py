72666f0db57b3da4cae0806cb4533e197ae241b3
Anna Pozdeeva <anna_pozdeeva1804@mail.ru>
2026-05-02 12:47:04 +0300

    Implement main function for file archiving and encryption

---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+from pathlib import Path
+
+from archivers import factory
+from archivers.archiver import ArchiverInterface
+from archivers.factory import outdir_suff
+from arg_parse.arg_parse import arg_parser
+from encryption import encryption
+from validating.encrypt import is_encrypted
+from validating.suff_validing import decompress as suff_val_dec
+
+
+def main():
+    parser = arg_parser()
+    args = parser.parse_args()
+    indir = args.indir
+    decrypted = False
+
+    if not Path(indir).exists():
+        print("Input directory does not exist")
+        return
+
+    if is_encrypted(indir):
+        in_path = Path(indir)
+        indir = indir[:-4]
+        out_path = Path(indir)
+        password = input("Password: ")
+        encryption.encrypt(in_path, out_path, password)
+        decrypted = True
+
+    in_path = Path(indir)
+
+    type_ = args.type
+    mode = args.mode
+
+    if mode == "decompress" and suff_val_dec(indir, type_):
+        return
+
+    outdir = outdir_suff(args.outdir if args.outdir else indir, type_, mode)
+    out_path = Path(outdir)
+    if out_path.exists():
+        out_path = Path("(1)" + outdir)
+
+    if mode:
+        archiver: ArchiverInterface = factory.create_archiver(type_)
+        if mode == "compress":
+            archiver.archive(in_path, out_path)
+        else:
+            archiver.unarchive(in_path, out_path)
+        in_path = out_path
+
+    password = True if args.setpassword == "True" else False
+
+    if not decrypted and password:
+        if mode == "decompress":
+            return
+        password = input("Password: ")
+        out_path = Path(outdir + ".enc")
+        encryption.encrypt(in_path, out_path, password)
+
+
+if __name__ == "__main__":
+    main()
