ab5c2cd3effceeb94e9f6f6d21ec0df4ff14a3a8
Anna Pozdeeva <anna_pozdeeva1804@mail.ru>
2026-05-02 12:39:46 +0300

    Add ArchiverInterface protocol definition

---
+++ archivers/archiver.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from typing import Protocol
+
+
+class ArchiverInterface(Protocol):
+    def archive(self, indir: Path, outdir: Path) -> None: ...
+
+    def unarchive(self, indir: Path, outdir: Path) -> None: ...
+      
